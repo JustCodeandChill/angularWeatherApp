7b1f639b7648fcfe1483d1ca01c11620e2edba31
JustCodeandChill <inforreceiver@gmail.com>
2022-11-03 15:20:00 -0400

    add routing feature

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ["ngRoute"]);
 
+myApp.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "pages/main.html",
+      controller: "mainController",
+    })
+    .when("/second/:num", {
+      templateUrl: "pages/second.html",
+      controller: "secondController",
+    })
+    .when("/second", {
+      templateUrl: "pages/second.html",
+      controller: "secondController",
+    });
+});
 myApp.controller("mainController", [
   "$scope",
   "$log",
-  "$filter",
-  "$timeout",
-  function ($scope, $log, $filter, $timeout) {
-    $scope.handle = "";
+  "$location",
+  function ($scope, $log, $location) {
+    console.log($location.path());
+    $scope.a = "Hello there";
 
-    $scope.lowercasehandle = function () {
-      return $filter("lowercase")($scope.handle);
-    };
+    $log.main = "this is mainb";
+    $log.log($log);
+  },
+]);
 
-    $scope.$watch("handle", function (newVAlue, oldValue) {
-      console.log("CChanged");
-      console.log("old value", oldValue);
-      console.log("new value", newVAlue);
-    });
-
-    $scope.characters = 5;
-
-    $scope.rules = [
-      {
-        rulename: "must be 5 vcharts",
-      },
-      {
-        rulename: "must be cool",
-      },
-      {
-        rulename: "must be stars",
-      },
-    ];
+myApp.controller("secondController", [
+  "$scope",
+  "$log",
+  "$location",
+  "$routeParams",
+  function ($scope, $log, $location, $routeParams) {
+    console.log($location.path());
+    console.log("route param", $routeParams);
+    $scope.a = $routeParams.num || 1;
+    $log.second = "this is second";
+    $log.log($log);
   },
 ]);
