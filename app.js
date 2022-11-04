6ba689f4a1829d40bf6eb0eb0dc17f9a1947e4df
JustCodeandChill <inforreceiver@gmail.com>
2022-11-03 22:37:49 -0400

    directives

--- app.js
+++ app.js
@@ -15,16 +15,42 @@ myApp.config(function ($routeProvider) {
       controller: "secondController",
     });
 });
+
+myApp.service("nameService", function () {
+  var self = this;
+  this.name = "John oe";
+  this.namelength = function () {
+    return self.name.length;
+  };
+});
+
 myApp.controller("mainController", [
   "$scope",
   "$log",
   "$location",
-  function ($scope, $log, $location) {
-    console.log($location.path());
-    $scope.a = "Hello there";
+  "nameService",
+  function ($scope, $log, $location, nameService) {
+    $scope.people = [
+      {
+        name: "Alex",
+        age: 12,
+        city: "haloi",
+        state: "HL",
+        zip: "12354",
+      },
+      {
+        name: "Kim",
+        age: 34,
+        city: "xinz",
+        state: "HL",
+        zip: "234",
+      },
+    ];
 
-    $log.main = "this is mainb";
-    $log.log($log);
+    $scope.formatFunction = function (person) {
+      return `${person.name} ${person.age} ${person.city}`;
+      //return "this is pain";
+    };
   },
 ]);
 
@@ -33,11 +59,41 @@ myApp.controller("secondController", [
   "$log",
   "$location",
   "$routeParams",
-  function ($scope, $log, $location, $routeParams) {
-    console.log($location.path());
-    console.log("route param", $routeParams);
-    $scope.a = $routeParams.num || 1;
-    $log.second = "this is second";
-    $log.log($log);
+  "nameService",
+  function ($scope, $log, $location, $routeParams, nameService) {
+    $scope.person = {
+      name: "Alex",
+      age: 12,
+      city: "haloi",
+      state: "HL",
+      zip: "12354",
+    };
+
+    $scope.formatFunction = function () {
+      //return `${person.name} ${person.age} ${person.city}`;
+      return "this is pain";
+    };
   },
 ]);
+
+myApp.directive("searchResult", function () {
+  return {
+    restrict: "AECM",
+    templateUrl: `directives/searchResult.html`,
+    replace: true,
+    scope: {
+      personObject2: "=personObject",
+      formatFunction: "&",
+    },
+    compile: function (elem, attrs) {
+      console.log("compiling ");
+      console.log(elem.html());
+      return {
+        post: function (scope, element, attrs) {
+          console.log("post-linking");
+          console.log(element, scope);
+        },
+      };
+    },
+  };
+});
